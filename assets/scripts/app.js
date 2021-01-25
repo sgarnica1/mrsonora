f5d2e885c961a6cc4756b0787474a2805beef0dc
Sergio Garnica <sergio190201@gmail.com>
2021-01-24 18:54:46 -0600

    Javascript mejorado, botón de visibilidad de contraseña agregado

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,69 +1,72 @@
-const submitBttn = document.getElementById('submit-bttn');
-submitBttn.addEventListener('click', (event) => validateInput(event))
+const form = document.getElementById('login-form');
+const userEmail = document.getElementById('email');
+const emailInput = document.getElementById('email');
+const passInput = document.getElementById('password');
+const emailLabel = document.getElementById('email__label');
+const emailContainer =  document.getElementById('email__container');
+const passContainer =  document.getElementById('password__container');
+const errorEmailMessage = document.querySelector('.login__error-message--email');
+const errorPassMessage = document.querySelector('.login__error-message--password');
+const passToggleTrigger = document.getElementById('password-toggle');
 
-
-const makeErrorMessageVisible = (element, input = '') => {
-  element.style.display = 'block';
-  input.classList.add('input-error');
-}
-const makeErrorMessageInvisible = (element, input) => {
-  element.style.display = 'none';
-  input.classList.remove('input-error');
-}
-
-// const validateEmail = (email, element, input) => {
-//   if(email ==='') {
-//     element.style.display = 'block';
-//     input.classList.add('input-error');
-//     return
-//   }
-//   const validation = /@/g
-//   const validEmail = validation.exec(email)
-//   if(validEmail) {
-//     element.style.display = 'none';
-//     input.classList.remove('input-error');
-//     return
-//   } else {
-//     element.style.display = 'block';
-//     input.classList.add('input-error');
-//   }
-// }
-
-const validateInput = event => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const logInForm = document.querySelector('.login__form')
-  const userEmail = document.getElementById('email');
-  const userPassword = document.getElementById('password');
-  const errorMessageEmail = document.querySelector('.login__error-message--email');
-  const errorMessagePassword = document.querySelector('.login__error-message--password');
-
-  if(userPassword.value == '' && userEmail.value == '') {
-    makeErrorMessageVisible(errorMessageEmail, userEmail);
-    errorMessageEmail.innerHTML = 'Introduzca una dirección de correo valida';
-    makeErrorMessageVisible(errorMessagePassword, userPassword);
-    errorMessagePassword.innerHTML = 'Contraseña incorrecta';
-  } else if(userPassword.value == '') {
-    makeErrorMessageInvisible(errorMessageEmail, userEmail);
-    errorMessageEmail.innerHTML = '';
-    makeErrorMessageVisible(errorMessagePassword, userPassword);
-    errorMessagePassword.innerHTML = 'Contraseña incorrecta';
-  } else if(userEmail.value == '') {
-    makeErrorMessageVisible(errorMessageEmail, userEmail);
-    errorMessageEmail.innerHTML = 'Introduzca una dirección de correo valida';
-    makeErrorMessageInvisible(errorMessagePassword, userPassword);
-    errorMessagePassword.innerHTML = '';
-  } else if(userPassword.value !== '' && userEmail.value !== '') {
-    makeErrorMessageInvisible(errorMessagePassword, userPassword);
-    makeErrorMessageInvisible(errorMessageEmail, userEmail);
-    errorMessageEmail.innerHTML = '';
-    errorMessagePassword.innerHTML = '';
-    logInForm.reset();
-    logIn();
+  let emailSuccesful =  false;
+  let passSuccesful =  false;
+  let emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+  if(!emailRegex.test(userEmail.value)) {
+    emailContainer.classList.add('error');
+    errorEmailMessage.innerHTML = 'Dirección de correo electrónico invalida';
+  } else {
+    emailContainer.classList.remove('error');
+    errorEmailMessage.innerHTML = '';
+    emailSuccesful = true;
   }
+  if(passInput.value.length < 8) {
+    passContainer.classList.add('error');
+    errorPassMessage.innerHTML = 'Contraseña invalida';
+  } else {
+    passContainer.classList.remove('error');
+    errorPassMessage.innerHTML = '';
+    passSuccesful = true;
+  }
+  if(emailSuccesful && passSuccesful) {
+    console.log('Logging in...');
+    form.reset();
+    inputNoFocus();
+  }
+})
 
+const inputFocus = () => {
+  userEmail.style.margin = '20px 0 5px 0';
+  emailLabel.style.top = '5px';
+  emailLabel.style.fontSize = '1.2rem';
+}
+const inputNoFocus = () => {
+  if(userEmail.value !== ''){
+    userEmail.style.margin = '20px 0 5px 0';
+    emailLabel.style.top = '5px';
+    emailLabel.style.fontSize = '1.2rem';
+  } else {
+    userEmail.style.margin = '10px 0 10px 0';
+    emailLabel.style.top = '13px';
+    emailLabel.style.fontSize = '1.5rem';
+  }
 }
 
-const logIn = () => {
-  console.log('Logging in...')
+emailInput.addEventListener('focus', inputFocus);
+emailInput.addEventListener('focusout', inputNoFocus);
+
+const passToggle = () => {
+  if (passInput.type === "password") {
+    passInput.type = "text";
+    passToggleTrigger.classList.remove('invisible');
+    passToggleTrigger.classList.add('visible');
+  } else {
+    passInput.type = "password";
+    passToggleTrigger.classList.add('invisible');
+    passToggleTrigger.classList.remove('visible');
+  }
 }
 
+passToggleTrigger.addEventListener('click', passToggle);
